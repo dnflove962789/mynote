f04df3a1e3c3aeb9c6a0b1eaf6cbdd07d1c2735d
zzr <470402780@qq.com>
2020-04-29 20:47:34 +0800

    用mybatisplus生成代码

---
+++ src/main/java/org/zzr/mynote/controller/EmailLogController.java
@@ -0,0 +1,20 @@
+package org.zzr.mynote.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author zzr
+ * @since 2020-04-29
+ */
+@RestController
+@RequestMapping("//email-log")
+public class EmailLogController {
+
+}
---
+++ src/main/java/org/zzr/mynote/service/IEmailLogService.java
@@ -0,0 +1,16 @@
+package org.zzr.mynote.service;
+
+import org.zzr.mynote.entity.EmailLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zzr
+ * @since 2020-04-29
+ */
+public interface IEmailLogService extends IService<EmailLog> {
+
+}
